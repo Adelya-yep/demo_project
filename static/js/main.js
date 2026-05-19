232fff3cd9d194a1fc3082fc770c8397a2b69597
Adelya-yep <ahasanova375@gmail.com>
2026-05-19 14:04:00 +0300

    Исправление ошибок

--- static/js/main.js
+++ static/js/main.js
@@ -1,35 +1,3 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const totalSlides = slides.length;
-let autoSlideInterval;
-
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-    });
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides;
-    showSlide(currentSlide);
-    resetAutoSlide();
-}
-
-function prevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-    showSlide(currentSlide);
-    resetAutoSlide();
-}
-
-function startAutoSlide() {
-    autoSlideInterval = setInterval(nextSlide, 3000);
-}
-
-function resetAutoSlide() {
-    clearInterval(autoSlideInterval);
-    startAutoSlide();
-}
-
 function maskPhone(input) {
     let digits = input.value.replace(/\D/g, '');
     if (input.value.startsWith('8')) {
@@ -59,11 +27,6 @@ function maskPhone(input) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    if (slides.length > 0) {
-        showSlide(0);
-        startAutoSlide();
-    }
-
     setTimeout(function() {
         const alerts = document.querySelectorAll('.alert-dismissible');
         alerts.forEach(function(alert) {
